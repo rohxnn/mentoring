d01986749b64fdb8f91ddf7a824e97dbcd20a17f
rohxnn <rohanreji123@gmail.com>
2026-01-14 12:50:04 +0530

    test(3.2.0): copy 3.1.2 changes

---
+++ documentation/3.2.0/dockerized/envs/env.js
@@ -0,0 +1,21 @@
+window["env"] = {
+	production: true,
+	name: 'prod environment',
+	staging: false,
+	dev: false,
+	baseUrl: 'http://localhost:3569',
+	sqliteDBName: 'mentoring.db',
+	deepLinkUrl: 'https://mentored.shikshalokam.org',
+	privacyPolicyUrl: 'https://shikshalokam.org/mentoring/privacy-policy',
+	termsOfServiceUrl: 'https://shikshalokam.org/mentoring/term-of-use',
+        recaptchaSiteKey: "",
+	recaptchaSiteKeyOld: "6LfWEKYpAAAAACxKbR7H42o3BwbJkJA06vIM_6Ea",
+	restictedPages: [],
+	unauthorizedRedirectUrl:"/auth/login",
+	supportEmail: 'example@org.com',
+	password: {
+		minLength: 10,
+		rejectPattern: '^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#%$&()\\-`.+,/]).{10,}$',
+		errorMessage: 'Password should contain at least one uppercase letter, one number and one special character.',
+	},
+  };
